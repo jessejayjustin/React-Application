78e7382873edba878bf5428c2dd9ffcc61c8a01d
Jesse Sosmena <diximobureros@gmail.com>
2019-09-25 15:00:26 +0800

    Add bootstrap into import

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
-import './../node_modules/bootstrap/dist/css/bootstrap.css';
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './scss/style.scss';
 
 render( 
