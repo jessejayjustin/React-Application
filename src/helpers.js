68750d87eb2183f48b91f39c4e0509c41ab907e9
Jesse Sosmena <diximobureros@gmail.com>
2019-10-27 16:46:28 +0800

    refactor: Use new syntax to calculate sum of an array of amount data

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,10 @@
-export const summaryDonations = (donations) => ( 
-  donations.reduce((accumulator, value) => (+accumulator + +value)) 
-);
+const summaryDonations = (data) => {
+  var sum=0; 
+  for(var i=0; i < data.length; i++) {
+    sum += data[i].amount
+  }
+  return sum;
+}
+
+export default summaryDonations;
+
