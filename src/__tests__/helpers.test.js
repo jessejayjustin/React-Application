d628dde8304a9c6e7c375fed4c9e4fc41f4d1c5d
Jesse Sosmena <diximobureros@gmail.com>
2019-10-27 15:27:06 +0800

    Drop unused test files

--- src/__tests__/helpers.test.js
+++
@@ -1,7 +0,0 @@
-import { summaryDonations } from '../helpers';
-
-describe('helpers', function() {
-  test('`summaryDonations` should calculate donations correctly', function() {
-    expect(summaryDonations([1, 2, 3, 5])).toEqual(11);
-  });
-});
